cc08f7e18f9ebc54767533395003f964b5b23e7c
Kevin Zolman <kevin.zolman@gmail.com>
2023-09-06 10:52:30 -0600

    Add functional #all_grades to gradebook

--- lib/gradebook.rb
+++ lib/gradebook.rb
@@ -33,4 +33,20 @@ class Gradebook
 
     student_list
   end
+
+  def all_grades
+    grade_list = Hash.new
+
+    @courses.each do |course|
+      course_grades = []
+
+      course.students.each do |student|
+        course_grades.concat(student.scores)
+      end
+
+      grade_list[course] = course_grades
+    end
+
+    grade_list
+  end
 end
