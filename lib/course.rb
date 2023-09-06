3cfce05e7e20ae632f3d067086072f1f11f7d056
Kevin Zolman <kevin.zolman@gmail.com>
2023-09-06 09:37:39 -0600

    Complete course and update tests

--- lib/course.rb
+++ lib/course.rb
@@ -1,3 +1,17 @@
 class Course
-  
+  attr_reader :name, :capacity, :students
+
+  def initialize(name, capacity)
+    @name = name
+    @capacity = capacity
+    @students = []
+  end
+
+  def full?
+    @students.count == @capacity
+  end
+
+  def enroll(student)
+    @students << student
+  end
 end
