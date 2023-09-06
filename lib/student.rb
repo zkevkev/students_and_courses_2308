9a2672a7b4e542201459db816c1bd4f6b964c7da
Kevin Zolman <kevin.zolman@gmail.com>
2023-09-06 09:24:18 -0600

    Initialize student, add #log_score and #grade

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,17 @@
 class Student
-  
+  attr_reader :name, :age, :scores
+
+  def initialize(student_info)
+    @name = student_info[:name]
+    @age = student_info[:age]
+    @scores = []
+  end
+
+  def log_score(score)
+    @scores << score
+  end
+
+  def grade
+    @scores.sum / @scores.count.to_f
+  end
 end
